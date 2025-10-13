a9b6364736c84c2871f32fe15da164e82c7dc23e
Negocios Tourinho <negocios.tourinho@gmail.com>
2025-10-13 20:53:16 -0300

    Força rebuild completo do projeto (dashboard.tsx

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,9 @@
 export default { reactStrictMode: true }
+const path = require("path");
+
+module.exports = {
+  webpack(config) {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
+};
